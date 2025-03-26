218d742c4bfd20772320de2beb35f69ce341512f
popov-val <popov.val@yahoo.com>
2025-03-26 17:45:05 +0300

    try profile

--- sprint_6/fin/b.py
+++ sprint_6/fin/b.py
@@ -1,73 +1,58 @@
 from collections import deque
-from line_profiler import profile
 
 
-@profile
-def get_number_plate(f, n, graph):
+def get_number_plate(n):
     empty_row = None
     yield empty_row
-    vers_cnt = 0
     for i in range(n):
-        row = []
-        for s in f.readline():
-            if s == '#':
-                vers_cnt += 1
-                row.append(vers_cnt)
-                graph.append([])
-            else:
-                row.append(s)
-        yield row
-    yield None
+        yield input()
+    yield empty_row
 
 
-@profile
-def add_to_graph(plate, graph, m):
+def add_to_graph(plate, graph, m, num_row):
     for i in range(m):
-        point = plate[1][i]
-        if point == '.':
+        if plate[1][i] == '.':
             continue
-
+        graph[(num_row, i)] = []
         for shift in range(-1, 2, 2):
             if 0 <= i + shift < m and plate[1][i + shift] != '.':
-                graph[point].append(plate[1][i + shift])
+                graph[(num_row, i)].append((num_row, i + shift))
             if plate[1 + shift] and plate[1 + shift][i] != '.':
-                graph[point].append(plate[1 + shift][i])
+                graph[(num_row, i)].append((num_row + shift, i))
 
 
-@profile
-def get_graph(f):
-    n, m = list(map(int, f.readline().split()))
+def get_graph():
+    n, m = list(map(int, input().split()))
     plate = deque()
-    graph = [None]
-    next_plate = get_number_plate(f, n, graph)
+    graph = {}
+    next_plate = get_number_plate(n)
     plate.append(next(next_plate))
     plate.append(next(next_plate))
-    i = n
-    while i:
-        i -= 1
+    num_row = 0
+    while num_row != n:
+        num_row += 1
         plate.append(next(next_plate))
-        add_to_graph(plate, graph, m)
+        add_to_graph(plate, graph, m, num_row)
         plate.popleft()
 
     return graph
 
 
-@profile
 def get_metrics(graph):
-    visited = [False for _ in range(len(graph))]
+    visited = set()
     max_color = 0
     max_cnt = 0
-    for v in range(1, len(graph)):
-        if visited[v]:
+    for v in graph.keys():
+        if v in visited:
             continue
         cnt = 0
         max_color += 1
         for_color = [v]
         while for_color:
             u = for_color.pop()
-            if visited[u]:
+            if u in visited:
                 continue
-            visited[u] = True
+            visited.add(u)
             cnt += 1
             for_color.extend(graph[u])
         max_cnt = max(cnt, max_cnt)
@@ -75,10 +60,8 @@ def get_metrics(graph):
     return max_color, max_cnt
 
 
-@profile
 def main():
-    with open('data/input.txt') as f:
-        graph = get_graph(f)
+    graph = get_graph()
     metrics = get_metrics(graph)
     print(*metrics)
 
