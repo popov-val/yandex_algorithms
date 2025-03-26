fefa2afbc0a9812a012f38403647f7e0e595459f
popov-val <popov.val@yahoo.com>
2025-03-26 22:33:01 +0300

    work version

--- sprint_6/fin/b.py
+++ sprint_6/fin/b.py
@@ -5,7 +5,7 @@ def main():
     cnt = 0
     max_size = 0
     n, m = list(map(int, input().split()))
-    shifts = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    shifts = [1, -1, m, -m]
     plate = [input() for _ in range(n)]
     d = deque()
     nums = n * m
@@ -17,22 +17,23 @@ def main():
         if plate[i][j] == '.':
             continue
 
-        d.append((i, j))
+        d.append(k)
         cnt += 1
         size = 0
         while d:
-            i, j = d.popleft()
+            num = d.popleft()
             size += 1
-            for shift_i, shift_j in shifts:
-                i_neib, j_neib = i + shift_i, j + shift_j
-                if 0 > i_neib or i_neib >= n or 0 > j_neib or j_neib >= m:
+            for shift in shifts:
+                neib = num + shift
+                if neib < 0 or neib >= nums:
                     continue
-                neib = i_neib * m + j_neib
+                if shift in [-1, 1] and neib // m != num // m:
+                    continue
                 if neib not in vers:
                     continue
                 vers.remove(neib)
-                if plate[i_neib][j_neib] == '#':
-                    d.append((i_neib, j_neib))
+                if plate[neib // m][neib % m] == '#':
+                    d.append(neib)
 
         max_size = max(size, max_size)
     return cnt, max_size
